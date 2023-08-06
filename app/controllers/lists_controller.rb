78da97ce6a8a4343ce95f85b2609251bfc83d60c
KozukaRyohei <138641952+KozukaRyohei@users.noreply.github.com>
2023-08-06 09:55:47 +0000

    [Finish] 3-9

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,9 +5,12 @@ class ListsController < ApplicationController
   end
 
   def create
-    list = List.new(list_params)
-    list.save
-    redirect_to list_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
   end
 
   def index
@@ -29,8 +32,8 @@ class ListsController < ApplicationController
   end
 
   def destroy
-    @list = List.find(params[:id])
-    @list.destroy
+    list = List.find(params[:id])
+    list.destroy
     redirect_to lists_path
   end
 
@@ -39,5 +42,4 @@ class ListsController < ApplicationController
   def list_params
     params.require(:list).permit(:title, :body, :image)
   end
- 
 end
